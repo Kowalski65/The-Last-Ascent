d6c384bf64ba50ec6ff193b3ec727d66da788884
Kowalski65 <nat1su32@gmail.com>
2025-01-12 10:54:38 +0200

    a

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class FollowCam : MonoBehaviour
 {
-    public Transform target; 
-    public Vector3 offset;   
-    public float smoothSpeed = 0.125f; 
+    public Transform target;
+    public float speed = 10f;
+
+    [Range(0f, 1f)]
+    public float smoothness = 0.3f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
 
     void LateUpdate()
     {
-        if (target != null)
-        {
-            
-            Vector3 desiredPosition = target.position + offset;
-            
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
-        }
+        var targetPosition = Vector3.MoveTowards(transform.position, target.position + offset, speed * Time.deltaTime);
+
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 1 - smoothness);
+
     }
 }
